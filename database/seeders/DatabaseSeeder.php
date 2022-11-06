15052709f2eec2b373493c9a6e6174ec6aad90cf
nmphi <minhphi03@gmail.com>
2022-11-06 14:43:03 +0700

    first add

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name' => 'CodeLean',
+                'email' => 'CodeLean@gmail.com',
+                'password' => Hash::make('123456'),
+                'avatar' => null,
+                'level' => 0,
+                'description' => null,
+            ],
+            [
+                'id' => 2,
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('123456'),
+                'avatar' => null,
+                'level' => 0,
+                'description' => null,
+            ],
+            [
+                'id' => 3,
+                'name' => 'Shane Lynch',
+                'email' => 'ShaneLynch@gmail.com',
+                'password' => Hash::make('123456'),
+                'avatar' => 'avatar-0.png',
+                'level' => 1,
+                'description' => 'Aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum bore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud amodo'
+            ],
+            [
+                'id' => 4,
+                'name' => 'Brandon Kelley',
+                'email' => 'BrandonKelley@gmail.com',
+                'password' => Hash::make('123456'),
+                'avatar' => 'avatar-1.png',
+                'level' => 1,
+                'description' => null,
+            ],
+            [
+                'id' => 5,
+                'name' => 'Roy Banks',
+                'email' => 'RoyBanks@gmail.com',
+                'password' => Hash::make('123456'),
+                'avatar' => 'avatar-2.png',
+                'level' => 1,
+                'description' => null,
+            ],
+        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
